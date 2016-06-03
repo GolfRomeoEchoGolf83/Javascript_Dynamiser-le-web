c105f57a95a8c4a09bc6b8c78936a186c1b497b7
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-06-03 17:32:47 +0200

    solution de l'exercice
    Ma solution de l’exercice.
    
    + correction de coquilles dans l’énoncé des paramètres.

--- chapitre_1/js/enfant.js
+++ chapitre_1/js/enfant.js
@@ -2,11 +2,23 @@
 
 // PARAMETRES :
 // Affiche un enfant d'un objet du DOM
-// Le paramètre noed est l'objet du DOM
-// Le paramètre indice est l'indicie de l'enfant à afficher
+// Le paramètre noeud est l'objet du DOM
+// Le paramètre indice est l'indice de l'enfant à afficher
+
+// todo : compléter la fonction
 function afficherEnfant(noeud, indice) {
-    // todo : compléter la fonction
+    if (noeud.nodeType === document.ELEMENT_NODE) {
+        if ((indice >= 0) && (indice < noeud.childNodes.length)) {
+            console.log(noeud.childNodes[indice]);
+        } else {
+            console.error("Indice incorrect");
+        }
+    } else {
+        console.error("Type de noeuf incorrect");
+    }
 }
+
+
 
 
 // PARAMETRE :
@@ -19,9 +31,19 @@ afficherEnfant(document.body, 1);
 afficherEnfant(document.body, -1);
 
 // Afficher l'erreur "indice incorrect"
-// Noeud body a moisn de 9 noeuds enfants
+// Noeud body a moins de 9 noeuds enfants
 afficherEnfant(document.body, 8);
 
 // Afficher l'erreur "type de noeud incorrect"
 // Premier noeud enfant de body est textuel et n'a pas d'enfant
 afficherEnfant(document.body.childNodes[0], 0);
+
+
+/* Compléter la fonction afficherEnfant pour obtenir :
+
+h1 
+Indice incorrect
+Indice incorrect
+Type de noeud incorrect
+
+*/

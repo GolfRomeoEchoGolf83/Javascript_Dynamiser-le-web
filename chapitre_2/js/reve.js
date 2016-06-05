5f2a5d4d74e43047f8fe09f0eba3a08c6677821b
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-06-05 19:25:24 +0200

    (JS) exercice 1
    Ajout du code JS de l’exercice 1 (avant solution)

---
+++ chapitre_2/js/reve.js
@@ -0,0 +1,6 @@
+// TODO: écrire la fonction compterElements
+
+console.log(compterElements("p")); // doit afficher 4
+console.log(compterElements(".adjeectif")); // doit afficher 3
+console.log(compterElements("p .adjectif")); // doit afficher 3
+console.log(compterElements("p > .adjectif")); // doit afficher 2

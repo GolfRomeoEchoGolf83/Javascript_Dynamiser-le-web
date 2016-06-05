723060238e49ada1caaa4ad98f50c2d56ffe5cf2
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2016-06-05 20:47:55 +0200

    (JS) solution de l'exercice
    Solution de l’exercice

--- chapitre_2/js/reve.js
+++ chapitre_2/js/reve.js
@@ -1,6 +1,13 @@
 // TODO: écrire la fonction compterElements
 
+/* Compter les éléments */
+
+function compterElements(selecteur) {
+    return document.querySelectorAll(selecteur).length;
+}
+
+
 console.log(compterElements("p")); // doit afficher 4
-console.log(compterElements(".adjeectif")); // doit afficher 3
+console.log(compterElements(".adjectif")); // doit afficher 3
 console.log(compterElements("p .adjectif")); // doit afficher 3
 console.log(compterElements("p > .adjectif")); // doit afficher 2
